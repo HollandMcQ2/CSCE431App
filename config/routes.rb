c040c12f5e5a8bc2cb2950372859cb7696169f75
root <root@localhost>
2022-02-13 21:47:37 +0000

    Events Index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   resources :home
   resources :users
+  resources :events
+  # resources :users, only [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
