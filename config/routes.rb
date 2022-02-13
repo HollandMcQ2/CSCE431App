bcfb6aa3fb1abb8b7b4643d1c2ef2f65c75bd9a3
root <root@localhost>
2022-02-13 20:48:19 +0000

    root set to home and home routing added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'users/index'
+  get 'home/index'
   resources :home
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
