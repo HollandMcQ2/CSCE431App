145b1548d51689c3c2344c58ffe385b4ba074730
root <root@localhost>
2022-02-15 03:01:26 +0000

    stylesheet fixed

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-// environment.plugins.prepend('Provide',
-//   new webpack.ProvidePlugin({
-//     $: 'jquery/src/jquery',
-//     jQuery: 'jquery/src/jquery'
-//   })
-// )
-
 module.exports = environment
