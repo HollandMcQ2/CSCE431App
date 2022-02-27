cc547f9aec4acc539174ea9f0283cb88cce70a2f
jmichaelknapp <jmichaelknapp@gmail.com>
2022-02-27 04:16:40 +0000

    Modifies User model to include the attributes is_current_member and has_paid_dues

--- db/migrate/20220227030737_update_users.rb
+++ db/migrate/20220227030737_update_users.rb
@@ -1,4 +1,6 @@
 class UpdateUsers < ActiveRecord::Migration[6.1]
-  def change
-  end
+	def change
+		add_column :users, :is_current_member, :boolean, null: false, default: true
+		add_column :users, :has_paid_dues, :boolean, default: false
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_101117) do
+ActiveRecord::Schema.define(version: 2022_02_27_030737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2022_02_14_101117) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "role", default: "member"
     t.bigint "attendance_count", default: 0, null: false
+    t.boolean "is_current_member", default: true, null: false
+    t.boolean "has_paid_dues", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
