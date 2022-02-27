1e690ad438484d0db5d0ea6bec029d8b62a47620
jmichaelknapp <jmichaelknapp@gmail.com>
2022-02-27 04:30:22 +0000

    Adds the Semester model and associated migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_27_030737) do
+ActiveRecord::Schema.define(version: 2022_02_27_042316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2022_02_27_030737) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password"
     t.bigint "attendance_count", default: 0, null: false
+  end
+
+  create_table "semesters", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start"
+    t.datetime "end"
+    t.float "duescost"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
