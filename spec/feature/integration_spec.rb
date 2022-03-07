08387dcbb31a84d5f1dcd19474171749cf6e11c2
Ninjaneer324 <ninjaflautist@tamu.edu>
2022-03-07 11:37:14 -0600

    testing

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Initial Page', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'shows Home Page' do
     visit new_user_session_url
-    expect(page).to have_content('Please Sign In')
+    expect(page).to have_content('Home')
 
   end
 end
