fa1217c76d17cbdde8b63b43f4cd1ddf7bec0511
Ninjaneer324 <ninjaflautist@tamu.edu>
2022-03-27 19:36:34 -0500

    CRUD functions initialized

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,4 +1,16 @@
 class HomePagesController < ApplicationController
     def index
     end
+    def show
+    end
+    def new
+      @homepage = HomePage.new
+      puts "New HomePage Contents"
+    end
+    def edit
+      @homepage = HomePage.find(params[:id])
+    end
+    def update
+      @homepage = HomePage.find(params[:id])
+    end
   end
--- app/models/home_page.rb
+++ app/models/home_page.rb
@@ -1,2 +1,7 @@
 class HomePage < ApplicationRecord
+    validates :slider_image_1, presence: true
+    validates :slider_image_2, presence: true
+    validates :slider_image_3: presence: true
+    validates :slider_image_4, presence: true
+    validates :slider_image_5, presence: true
 end
