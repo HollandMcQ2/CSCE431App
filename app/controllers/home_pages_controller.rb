d720f512264002261a73428ebd858d440ad9709b
root <root@localhost>
2022-03-30 01:57:30 +0000

    update incomplete

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -8,5 +8,18 @@ class HomePagesController < ApplicationController
     end
     def update
       @home_page = HomePage.find(params[:id])
+      respond_to do |format|
+        if @home_page.update(home_page_params)
+          format.html { redirect_to home_pages_url, notice: "home_page was successfully updated." }
+          format.json { render :show, status: :ok, location: @home_page }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @home_page.errors, status: :unprocessable_entity }
+        end
+      end
     end
+    private
+      def home_page_params
+        params.require(:home_page).permit(:heading, :summary)
+      end
   end
--- app/models/home_page.rb
+++ app/models/home_page.rb
@@ -1,7 +1,4 @@
 class HomePage < ApplicationRecord
-    validates :slider_image_1, presence: true
-    validates :slider_image_2, presence: true
-    validates :slider_image_3, presence: true
-    validates :slider_image_4, presence: true
-    validates :slider_image_5, presence: true
+    validates :heading, presence: true
+    validates :summary, presence: true
 end
