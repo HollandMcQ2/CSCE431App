2752d4cb2ed6a279cfa2b9ae95aef07b8fcabb8c
root <root@localhost>
2022-03-31 00:41:12 +0000

    seeded HomePage

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(:full_name => 'Ken Kirkley', :email => "kenkirkley1330@gmail.com", :role => "admin");
-User.create(:full_name => 'Holland McQuaid', :email => "HollandMcQ2@gmail.com", :role => "admin", :attendance_count => 1, :has_paid_dues => true);
-User.create(:full_name => 'Elaine Mo', :email => "kakashiandflute@gmail.com", :role => "admin");
-User.create(:full_name => 'John Knapp', :email => "jmichaelknapp@gmail.com", :role => "admin");
-User.create(:full_name => 'John Tester', :email => "t3ster219@gmail.com", :role => "admin");
-User.create(:full_name => 'Johnathan Torres', :email => "torres404@tamu.edu", :role => "admin");
-Semester.create(:name => 'Spring 2022', :start => "1/22/22", :end=> "5/14/22", :duescost => 200.00)
+# User.create(:full_name => 'Ken Kirkley', :email => "kenkirkley1330@gmail.com", :role => "admin");
+# User.create(:full_name => 'Holland McQuaid', :email => "HollandMcQ2@gmail.com", :role => "admin", :attendance_count => 1, :has_paid_dues => true);
+# User.create(:full_name => 'Elaine Mo', :email => "kakashiandflute@gmail.com", :role => "admin");
+# User.create(:full_name => 'John Knapp', :email => "jmichaelknapp@gmail.com", :role => "admin");
+# User.create(:full_name => 'John Tester', :email => "t3ster219@gmail.com", :role => "admin");
+# User.create(:full_name => 'Johnathan Torres', :email => "torres404@tamu.edu", :role => "admin");
+# Semester.create(:name => 'Spring 2022', :start => "1/22/22", :end=> "5/14/22", :duescost => 200.00)
 
-Event.create(:name => 'Test Event', :password => "testEventPassword", :description => 'This is a test event', :time => '2022-03-04', :link => "www.google.com", :location => 'Test Location', :is_mandatory => true, :is_recurring => false);
-Event.create(:name => 'Test Event2', :password => "password", :description => 'This is a test event', :time => '2022-03-05', :link => "www.google.com", :location => 'Test Location2', :is_mandatory => true, :is_recurring => false);
-Event.create(:name => 'Test Event3', :password => "password", :description => 'This is a test event', :time => '2022-03-06', :link => "www.google.com", :location => 'Test Location3', :is_mandatory => true, :is_recurring => false);
-EventUser.create(:user_id => 2, :event_id => 1, :attended => true);
+# Event.create(:name => 'Test Event', :password => "testEventPassword", :description => 'This is a test event', :time => '2022-03-04', :link => "www.google.com", :location => 'Test Location', :is_mandatory => true, :is_recurring => false);
+# Event.create(:name => 'Test Event2', :password => "password", :description => 'This is a test event', :time => '2022-03-05', :link => "www.google.com", :location => 'Test Location2', :is_mandatory => true, :is_recurring => false);
+# Event.create(:name => 'Test Event3', :password => "password", :description => 'This is a test event', :time => '2022-03-06', :link => "www.google.com", :location => 'Test Location3', :is_mandatory => true, :is_recurring => false);
+# EventUser.create(:user_id => 2, :event_id => 1, :attended => true);
 
 HomePage.create(:heading => 'Howdy', :summary => 'Hello');
