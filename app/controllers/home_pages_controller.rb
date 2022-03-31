c5d9ad12a4114c013bef696a7598876243f1e909
Ninjaneer324 <ninjaflautist@tamu.edu>
2022-03-30 19:14:09 -0500

    reverted one commit

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,26 +1,16 @@
 class HomePagesController < ApplicationController
     def index
-      @home_page = HomePage.find(1)
     end
     def show
     end
+    def new
+      @homepage = HomePage.new
+      puts "New HomePage Contents"
+    end
     def edit
-      @home_page = HomePage.find(1)
+      @homepage = HomePage.find(params[:id])
     end
     def update
-      @home_page = HomePage.find(1)
-      respond_to do |format|
-        if @home_page.update(home_page_params)
-          format.html { redirect_to home_pages_url, notice: "home_page was successfully updated." }
-          format.json { render :show, status: :ok, location: @home_page }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @home_page.errors, status: :unprocessable_entity }
-        end
-      end
+      @homepage = HomePage.find(params[:id])
     end
-    private
-      def home_page_params
-        params.require(:home_page).permit(:heading, :summary)
-      end
   end
