e86896e5aa3a85872694293f14d8d46afd6679a9
root <root@localhost>
2022-04-01 20:21:58 +0000

    figure out how to pass parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -131,4 +131,18 @@ class UsersController < ApplicationController
     puts "I am user @view_meetings: #{@user.id}"
   end
 
+  def edit_role
+    if current_user.role == 'admin'
+      @user = User.find(params[:id])
+      respond_to do |format|
+        if @user.update(edit_role_params)
+          format.html { redirect_to users_url, notice: "User role was successfully updated." }
+        end
+      end
+    end
+  end
+  private
+  def edit_role_params
+    params.require(:user).permit(:role)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :users do
     member do
       get :meetings
+      get :edit_role
       get :payment
       get :thank_you, to: 'users/:id/thank_you'
       post :checkout
