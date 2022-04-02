58bc35290577f1627d050cf57b67ddac6b1ac07b
jmichaelknapp <jmichaelknapp@tamu.edu>
2022-04-02 00:29:42 +0000

    Implements CRUD for semesters

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -8,20 +8,55 @@ class SemestersController < ApplicationController
 	end
 
 	def new
+		@semester = Semester.new
 	end
 
 	def create
+		@semester = Semester.new(semester_params)
+		respond_to do |format|
+			if @semester.save
+				format.html {redirect_to semesters_url, notice: "Semester was successfully created."}
+				format.json {render :show, status: :created, location: @semester}
+			else
+				format.html {render :new, status: :unprocessable_entity}
+				format.json {render json: @semester.errors, status: :unprocessable_entity}
+			end
+		end
 	end
 
 	def edit
+		@semester = Semester.find(params[:id])
 	end
 
 	def update
+		@semester = Semester.find(params[:id])
+		respond_to do |format|
+			if @semester.update(semester_params)
+				format.html {redirect_to semesters_url, notice: "Semester was successfully updated."}
+				format.json {render :show, status: :ok, location: @semester}
+			else
+				format.html {render :edit, status: :unprocessable_entity}
+				format.json {render json:@semester.errors, status: :unprocessable_entity}
+			end
+		end
 	end
 
 	def delete
+		@semester = Semester.find(params[:id])
 	end
 
 	def destroy
+		@semester = Semester.find(params[:id])
+		@semester.destroy
+		respond_to do |format|
+			format.html {redirect_to semesters_url, notice: "Semester was successfully destroyed."}
+			format.json {head :no_content}
+		end
 	end
+
+	private
+		def semester_params
+			params.require(:semester).permit(:name,:start,:end,:duescost)
+		end
+
 end
--- spec/views/semesters/index.html.erb_spec.rb
+++ spec/views/semesters/index.html.erb_spec.rb
@@ -14,10 +14,23 @@ RSpec.describe "semesters/index.html.erb", type: :view do
 				end: Date.new(2022,5,7)
 			)
 		])
+		assign(:users, [
+			User.create!(
+				full_name: "Jimbo Tester",
+				email: "cringe@gmail.com",
+				id: 1,
+				role: "admin"
+			)
+		])
+		assign(:current_user, User.first)
 	end
 	
 	it "renders a list of Semesters" do
 		render
 		assert_select "tr", count: Semester.all.size+1 #each row plus the header
+	end
+	it "has a link back to the dashboard" do
+		render
+		expect(rendered).to include("<a class=\"mt-5\" href=\"/users/"+@user.id.to_s+"\">")
 	end
 end
