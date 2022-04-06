389bd56cd6500a6a5f8079f7aa59bf1b2b62d97f
jmichaelknapp <jmichaelknapp@tamu.edu>
2022-04-06 03:38:08 +0000

    Makes controller save valid submissions to the database, reject invalid ones, and give feedback either way; makes submission redirect to home page; makes model validate presence of message

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -4,7 +4,16 @@ class ContactFormsController < ApplicationController
 	end
 
 	def create
-		
+		@contact_form = ContactForm.new(form_params)
+		respond_to do |format|
+			if @contact_form.save
+				format.html { redirect_to root_url, notice: "Message has been sent." }
+        		format.json { render :show, status: :created, location: @contact_form }
+			else
+				format.html {render :new, status: :unprocessable_entity}
+				format.json {render json: @contact_form.errors, status: :unprocessable_entity}
+			end
+		end
 	end
 
 	def index
