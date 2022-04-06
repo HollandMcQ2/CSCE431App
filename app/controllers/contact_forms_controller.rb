b64be433d5331d8e53b4cbb058a064284b35aa1e
jmichaelknapp <jmichaelknapp@tamu.edu>
2022-04-06 05:17:51 +0000

    Implements delet for Contact Us messages

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -17,12 +17,21 @@ class ContactFormsController < ApplicationController
 	end
 
 	def index
+		@forms = ContactForm.order(created_at: :desc)
 	end
 
 	def show
+		@form = ContactForm.find(params[:id])
 	end
 
 	def destroy
+		@form = ContactForm.find(params[:id])
+		@form.destroy
+
+		respond_to do |format|
+			format.html {redirect_to contact_forms_url, notice: "Message was successfully deleted."}
+			format.json {head :no_content}
+		end
 	end
 
 	private
